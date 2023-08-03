1fa72a76e5ea71bb43618710ab9a890dce15ecaf
emontilla07 <emontilla107@gmail.com>
2023-08-03 16:40:29 -0500

    Clean up all project and started again

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,21 +1,9 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react"
-import Formulario from "./components/Formulario"
-import Header from "./components/Header"
-import ListadoPacientes from "./components/ListadoPacientes"
-
 function App() {
-    const [ patients, setPatients ] = useState([]);
-    const [ patient, setPatient ] = useState({});
-
     return (
-        <div className="container mx-auto w-11/12 mt-20">
-            <Header />
-            <div className="mt-12 md:flex">
-                <Formulario patients={ patients } setPatients={ setPatients } patient={ patient } />
-                <ListadoPacientes patients={ patients } setPatient={ setPatient } />
-            </div>
-        </div>
+        <>
+            
+        </>
     )
 }
 
